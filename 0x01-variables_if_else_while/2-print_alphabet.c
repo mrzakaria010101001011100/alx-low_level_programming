501bdc2f5a4625c010188b56f79f73066ff11a9c
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2023-10-06 16:46:40 -0700

    base-shell

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * entre point alphabet
+ *
+ * we can use putchar in output
+ *
+ * alwys RETURN 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+{
+	putchar(ch);
+	 a++;
+	putchar('\n');
+}
+return (0);
+}
+
+
