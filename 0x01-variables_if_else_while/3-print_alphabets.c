dfed67eaefb8d8ed0483248b41a130faf05ffc22
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2023-10-06 17:19:51 -0700

    base-shell

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: print all alphabet letters lawercase and uppercase
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	/*prints a-z*/
+        while (ch <= 'z')
+{
+        putchar(ch);
+        ch++;
+}
+
+	/*prints A-Z*/
+        while (CH <= 'Z')
+{
+        putchar(CH);
+        CH++;
+}
+        putchar('\n');
+return (0);
+}
+
