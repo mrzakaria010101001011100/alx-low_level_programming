2e51255b52067efe42007e83352860b79760891e
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2023-10-07 06:01:44 -0700

    base-shell

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -prints single digit numbers of base 10
+ *
+ * starting for 0.
+ *
+ * return: Always 0.
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (n < 10; n++)
+		printf("%d", num);
+
+	printf("\n");
+	return (0);
+}
