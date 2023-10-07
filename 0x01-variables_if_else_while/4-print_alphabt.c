d05914803c353faa967d5b4edabfedbd6c691ec6
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2023-10-07 04:50:28 -0700

    base-shell

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,28 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
 /**
- * main  - prints the alphabet in lowercase, except for q and e.
- * 
- * return: always 0.
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	char letter;
 
-	/*prints a - z except q and e*/
 	for (letter = 'a'; letter <= 'z'; letter++)
-{
+	{
 		if (letter != 'e' && letter != 'q')
 			putchar(letter);
-}
+	}
+
 	putchar('\n');
-return (0);
+
+	return (0);
 }
 
 
