ce2822f0dc87fc9cd36f235cefae4411de2dde6a
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2023-10-07 06:26:36 -0700

    base-shell

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,15 +10,14 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	int n;
+	int num;
 
-	for (n = 0;n < 10; n++)
-		printf("%d", n);
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
 
 	printf("\n");
 
-return (0);
+	return (0);
 }
