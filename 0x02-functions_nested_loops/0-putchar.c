8d3baa55fde383c9a9e6c9882914af27dab544d5
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2023-10-10 06:56:03 -0700

    base-shell

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry to write a program that print -putchar
+ *
+ * Return Always 0
+ */
+
+int main(void)
+{
+	putchar('_putchar');
+	putchar('\n');
+return (0);
+}
