943be2056f948b7eef0897254c29080ca5c133a3
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2023-10-13 10:45:11 -0700

    base-shell

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - chech if c is upper
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if its uppercase. 0 if not
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && C <= 90)
+		return (1);
+	else
+		return (0);
+}
