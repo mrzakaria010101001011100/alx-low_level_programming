bb3267346703417d47443b0011e4b926072eee14
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2023-10-17 16:05:38 -0700

    base-shell

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,2 +1,26 @@
 #include "main.h"
 
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: nothing
+*/
+
+void rev_string(char *s);
+{
+	int l, i;
+	char temp;
+
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+
+	for (i = 0; i < 1 / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
+}
+
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ *
+ * @dest: char type string
+ * @src: char type string
+ * Return: pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src);
+{
+	int i = -1;
+
+	do {
+		i++
+		dest[i] = srt[i];
+	} while (src[i] != '\n');
+	return (dest);
+}
+
