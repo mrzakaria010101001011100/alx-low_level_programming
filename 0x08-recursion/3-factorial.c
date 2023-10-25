4c9ff715d21ab3eb1cb3f3a3e0a66ccff1065eeb
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2023-10-25 14:34:49 -0700

    base-shell

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * factorial - function parameter
- *
- * @n:function parameter
- *
- * Return: Function n.
- */
+  * factorial - function parameter
+  *
+  * @n: function parameter
+  *
+  * Return: Function n.
+  */
 int factorial(int n)
 {
-	if(n < 0)
-		return(-1);
-	if(n == 0)
-		return(1);
-		return(n * factorial(n - 1));
+	if (n < 0)
+	return (-1);
+	if (n == 0)
+	return (1);
+	return (n * factorial(n - 1));
 }
+
