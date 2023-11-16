2ec8832515bb989be34d5eea03fa63fc855c4bad
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2023-11-16 13:20:59 -0800

    base-shell

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <list.h>
+#include <lists.h>
 
 /**
  * list_len - returns the number of elements in a linked list
@@ -9,12 +9,12 @@
  */
 size_t list_len(const list_t *h);
 {
-	size_t n = 0;
+        size_t n = 0;
 
-	while (h)
-	{
-		n++;
-		h = h->next;
-	}
+        while (h)
+        {
+                n++;
+                h = h->next;
+        }
 return (n);
 }
